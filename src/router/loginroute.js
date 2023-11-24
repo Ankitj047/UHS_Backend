a977ffac98564201fd00580da3e392c4363618ef
Ankit Jain <ankit.jain@ifour.com>
2023-11-24 12:23:34 +0530

    develop

--- src/router/loginroute.js
+++ src/router/loginroute.js
@@ -1,34 +1,38 @@
 const express = require("express");
-const bcrypt = require("bcrypt")
-const loginroute = new express.Router;
-const User = require("../Models/register")
-var jwt = require('jsonwebtoken');
+const bcrypt = require("bcrypt");
+const loginroute = new express.Router();
+const User = require("../Models/register");
+var jwt = require("jsonwebtoken");
 
-loginroute.post("/login", async (req,res)=> {
-try {
+loginroute.post("/login", async (req, res) => {
+  try {
     const email = req.body.emailvalue;
-    const password = req.body.pass
-  const useremailverify =  await User.findOne({email: email})
+    const password = req.body.pass;
+    const useremailverify = await User.findOne({ email: email });
 
-  const isMatch = await bcrypt.compare(password, useremailverify.pass)
+    if (useremailverify !== null) {
+      const isMatch = await bcrypt.compare(password, useremailverify.pass);
+      if (isMatch) {
+        const token1 = jwt.sign(
+          {
+            userId: useremailverify._id,
+            email: useremailverify.email,
+          },
+          process.env.SECRET_KEY,
+          { expiresIn: "1d" }
+        );
 
-  const token1 = jwt.sign({
-    userId: useremailverify._id , email : useremailverify.email
-}, process.env.SECRET_KEY, {expiresIn: "1d"});
+        res
+          .status(200).send({id: useremailverify._id, messaage: "successful", token: token1});
+      } else {
+        res.status(400).send({ messaage: "Invalid password" });
+      }
+    } else {
+      res.status(400).send({ messaage: "Invalid mail" });
+    }
+  } catch (error) {
+    res.send(error?.message);
+  }
+});
 
-if (useremailverify !== null){
-if(isMatch){
-    res.status(200).send({ id :useremailverify._id, messaage: "successful", token: token1})
-}
-else{
-    res.status(400).send("Invalid password")
-}}
-else {
-res.send("Invalid mail")
-}
-} catch (error) {
-    res.send(error?.message)
-
-}})
-
-module.exports = loginroute
+module.exports = loginroute;

06d172d5f4b77c9ccdf6bffa09b64bea38b7e654
Ankit Jain <ankit.jain@ifour.com>
2023-12-01 18:55:54 +0530

    rozerpay udpated

--- src/router/paymentGatewayRoute.js
+++ src/router/paymentGatewayRoute.js
@@ -24,7 +24,6 @@ paymentGatewayDataRoute.post("/paymentGatewayTest", async (req, res) => {
 
 paymentGatewayDataRoute.post("/checkout", async (req, resp) => {
   try {
-
     const options = {
       amount: Number(req.body.amount * 100), // amount in the smallest currency unit so will chnage paise to rupee
       currency: "INR",
@@ -32,9 +31,39 @@ paymentGatewayDataRoute.post("/checkout", async (req, resp) => {
     };
 
     const order = await instance.orders.create(options);
-    resp.status(200).send({ message: "succesfull" });
+    return resp.status(200).send({ message: "succesfull", order });
   } catch (error) {
     console.log(error.message);
+  }
+});
+
+paymentGatewayDataRoute.post("/paymenetVerification", async (req, res) => {
+  try {
+    console.log(req.body, "req.body")
+
+    const {razorpay_payment_id, razorpay_order_id,razorpay_signature } = req.body
+
+  //  const generated_signature = hmac_sha256(order_id + "|" + razorpay_payment_id, secret);
+
+//   if (generated_signature == razorpay_signature) {
+//     return res.status(200).send({ message: "payment is successful"}, {"req" :req.body}); 
+//   }
+// else{
+//   return res.status(400).send({message: "Fake call"})
+// }
+
+return res.status(200).send({ message: "payment is successful"}); 
+
+  } catch (error) {
+    console.log(error?.message);
+  }
+});
+
+paymentGatewayDataRoute.get("/keyID", async (req, res) => {
+  try {
+    res.status(200).send({ key: instance.key_id });
+  } catch (error) {
+    console.log(error?.message);
   }
 });
 
